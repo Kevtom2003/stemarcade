522161d3e6581b59a5c684e3038cb1c3cab18ced
dgshanee <shaneciardelli27@gmail.com>
2024-01-27 17:58:43 -0600

    Teacher login

--- stemarcade/src/components/Home.js
+++ stemarcade/src/components/Home.js
@@ -1,7 +1,8 @@
 import TeacherSelect from "./TeacherSelect";
+import TeacherLogin from "./TeacherLogin";
 
 export default function Home(){
     return(
-        <TeacherSelect/>
+        <TeacherLogin/>
     )
 }
---
+++ stemarcade/src/components/StudentLogin.js
@@ -0,0 +1,10 @@
+export default function StudentLogin(){
+    return(
+        <div>
+            <h1>Full name</h1>
+            <input></input>
+            <h1>Class ID</h1>
+            <input></input>
+        </div>
+    )
+}
