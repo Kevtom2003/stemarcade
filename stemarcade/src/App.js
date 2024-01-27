3c3b9a6a21ac2f86f6b8e7f752dd2badaf89bb2d
Kevtom2003 <kevtom2003@gmail.com>
2024-01-27 16:55:42 -0600

    added screen

--- stemarcade/src/App.js
+++ stemarcade/src/App.js
@@ -1,9 +1,17 @@
-import { Stage } from '@inlet/react-pixi/index'
+import { Stage, Sprite } from '@inlet/react-pixi/index'
 import './App.css';
+import wizard from "./wizard.png"
 
 const App = () => {
   return(
-  <Stage>
+  <Stage
+  width={300} 
+  height={300} 
+  options={{ 
+  backgroundColor: 0x012b30, 
+  antialias: true 
+  }}>
+    <Sprite image={wizard} x={150} y={150}/>
   </Stage>
 )}
 
