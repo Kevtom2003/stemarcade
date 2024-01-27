518a3d55383e94763e222e7d325c29472f3ddb0b
Kevtom2003 <kevtom2003@gmail.com>
2024-01-27 16:37:42 -0600

    sprite setup

--- stemarcade/src/App.js
+++ stemarcade/src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import Stage from "@inlet/react-pixi"
+import { Stage } from '@inlet/react-pixi/index'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return(
+  <Stage>
+  </Stage>
+)}
 
 export default App;
