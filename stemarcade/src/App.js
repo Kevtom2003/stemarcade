a17e4097e2301171c82f0fe104bcbb26d87c691e
dgshanee <shaneciardelli27@gmail.com>
2024-01-27 17:21:51 -0600

    TeacherLogin.js

--- stemarcade/src/App.js
+++ stemarcade/src/App.js
@@ -1,18 +1,11 @@
 import { Stage, Sprite } from '@inlet/react-pixi/index'
 import './App.css';
+import Home from './components/Home';
 import wizard from "./wizard.png"
 
 const App = () => {
   return(
-  <Stage
-  width={300} 
-  height={300} 
-  options={{ 
-  backgroundColor: 0x012b30, 
-  antialias: true 
-  }}>
-    <Sprite image={wizard} x={150} y={150}/>
-  </Stage>
+  <Home/>
 )}
 
 export default App;
