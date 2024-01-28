de80024cceabe166f1ade14b36e6ba50a1d737e8
dgshanee <shaneciardelli27@gmail.com>
2024-01-27 19:24:27 -0600

    Routing update

--- stemarcade/src/App.js
+++ stemarcade/src/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import './App.css';
 import {Routes,Route} from "react-router-dom"
 
-import Home from "./components/teacherHome";
-import TeacherSelect from './components/TeacherSelect';
-import Login from './components/Login';
+import HomePage from './components/HomePage'
 
 const App = () => {
   return(
   <Routes>
-    <Route path="/home" element={<Home />} />
-    <Route path="/teacherselect" element={<TeacherSelect />} />
-    <Route path="/login" element={<Login/>}/>
+    <Route path = "/" element = {<HomePage/>}/>
   </Routes>
   )
 }
--- stemarcade/src/components/Home.js
+++
@@ -1,8 +0,0 @@
-import TeacherSelect from "./TeacherSelect";
-import TeacherLogin from "./TeacherLogin";
-
-export default function Home(){
-    return(
-        <TeacherLogin/>
-    )
-}
