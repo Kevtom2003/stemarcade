ad22392e74045c476a09f8008875f8453b1d8383
aciardelli <anthciardelli@gmail.com>
2024-01-27 23:12:52 -0600

    Added teacher page

--- stemarcade/src/components/TeacherLogin.js
+++ stemarcade/src/components/TeacherLogin.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import InputField from './InputField';
+import { Navigate } from 'react-router-dom';
 
 export default function TeacherLogin(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [loggedIn, setLoggedIn] = useState(false);
 
     // const handleKeyDown = (event) => {
     //     if(event.keyCode === 13 && username != "" && password != ""){
@@ -25,25 +27,30 @@ export default function TeacherLogin(){
             const res = await fetch(`http://localhost:5000/api/teacher/${username}/${password}`);
             const p = await res.json();
             if(p.length > 0){
-                handleLogin();
+                setLoggedIn(true);
             }
             console.log(p);
         }catch(err){
             console.error(err);
         }
+
+        if (loggedIn) {
+            Navigate(`/teacher`);
+        }
     }
     
-    const handleLogin = () => {
-        console.log("Logging in...");
-    }
+    // const handleLogin = () => {
+    //     console.log("Logging in...");
+    //     Redirect("/teacher/:username/:password");
+    // }
 
     return(
         <div>
             <h1>Username</h1>
-            <InputField/>
-            {/* <input placeholder="teacher username" onChange={(e) => {setUsername(e.target.value)}}></input> */}
+            {/* <InputField/> */}
+            <input placeholder="teacher username" onChange={(e) => {setUsername(e.target.value)}}></input>
             <h1>Password</h1>
-            {/* <input placeholder="teacher password" onChange = {(e) => {setPassword(e.target.value)}}></input> */}
+            <input placeholder="teacher password" onChange = {(e) => {setPassword(e.target.value)}}></input>
             <button onClick={handleEnter}>Press me</button>
         </div>
     )
--- stemarcade/src/components/TeacherOverview.js
+++ stemarcade/src/components/TeacherOverview.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import ArcadeOutline from "./ArcadeOutline";
+import './TeacherOverview.css';
 
 export default function TeacherOverview(){
+
+    const [classList, setClassList] = useState(['Nothing here yet']);
+    const [assignmentList, setAssignmentList] = useState(['Nothing here yet']);
+
+    const [classSelect, setClassSelect] = useState("");
+    const [assignmentSelect, setAssignmentSelect] = useState("");
+
+    useEffect(() => {
+        fetch(`http:localhost:5000/api/class/`)
+        .then((res) => res.json())
+        .then((data) => {
+            setClassList(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, [])
+
+    useEffect(() => {
+        fetch(`http:localhost:5000/api/assignment/${classSelect}`)
+        .then((res) => res.json())
+        .then((data) => {
+            setAssignmentList(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, [classSelect])
+
+
     return(
-        <ArcadeOutline style = {{display:"flex", justifyContent: "center", alignItems: "center"}}>
+        <ArcadeOutline >
             <div className = "screen">
-                <h1>Teacher Overview</h1>
+                <h1>Teacher Dashboard</h1>
+                <div className = "select-buttons">
+                    <div className = "select-package">
+                        <label>Class Select</label>
+                        <select className="teacher-select">
+                            {classList.map((classItem) => (
+                                <option value={classItem.class_id}>{classItem.class_name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className = "select-package">
+                        <label>Assignment Select</label>
+                        <select className="teacher-select">
+                        </select>
+                    </div>
+                </div>
+                <div className="student-leaderboard">
+                    <h2>Student Leaderboard</h2>
+                    <table className="student-table">
+                        <tr>
+                            <th>Student</th>
+                            <th>Score</th>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Jane Doe</td>
+                            <td>90</td>
+                        </tr>
+                    </table>
+                </div>
+
+
             </div>
         </ArcadeOutline>
     )
