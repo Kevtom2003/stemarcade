d3db35865fb4b641c5efac8c4c89060189f8dfd4
dgshanee <shaneciardelli27@gmail.com>
2024-01-27 19:32:05 -0600

    Fix routing

--- stemarcade/src/App.js
+++ stemarcade/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import './App.css';
 import {Routes,Route} from "react-router-dom"
 import HomePage from './components/HomePage'
+import GalagaGame from "./components/galagaGame";
 
 const App = () => {
   return(
   <Routes>
     <Route path = "/" element = {<HomePage/>}/>
+    <Route path = "/galaga" element = {<GalagaGame/>}/>
   </Routes>
   )
 }
