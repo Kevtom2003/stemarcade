7a0ad2f283ed6105d960c478b87f8b99085dcd6d
dgshanee <shaneciardelli27@gmail.com>
2024-01-27 20:57:00 -0600

    hey

--- stemarcade/src/components/TeacherLogin.js
+++ stemarcade/src/components/TeacherLogin.js
@@ -5,22 +5,26 @@ export default function TeacherLogin(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    // const handleKeyDown = (event) => {
-    //     if(event.keyCode === 13 && username != "" && password != ""){
-    //         console.log("submit");
-    //     }
-    // }
+    const handleKeyDown = (event) => {
+        if(event.keyCode === 13){
+            console.log(username, password);
+        }
+        if(event.keyCode === 13 && username != "" && password != ""){
+            handleEnter();
+        }
+    }
 
-    // useEffect(() => {
-    //     window.addEventListener('keydown', handleKeyDown);
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
 
-    //     return () => {
-    //         window.removeEventListener('keydown', handleKeyDown);
-    //     }
-    // }, []);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    }, []);
 
 
     const handleEnter = async () => {
+        console.log("hey there");
         try{
             const res = await fetch(`http://localhost:5000/api/teacher/${username}/${password}`);
             const p = await res.json();
@@ -40,9 +44,10 @@ export default function TeacherLogin(){
     return(
         <div>
             <h1>Username</h1>
-            <InputField/>
+            <InputField focus = {true} onChange = {(e) => {setUsername(e.target.value)}}/>
             {/* <input placeholder="teacher username" onChange={(e) => {setUsername(e.target.value)}}></input> */}
             <h1>Password</h1>
+            <InputField onChange = {(e) => {setPassword(e.target.value)}}/>
             {/* <input placeholder="teacher password" onChange = {(e) => {setPassword(e.target.value)}}></input> */}
             <button onClick={handleEnter}>Press me</button>
         </div>
